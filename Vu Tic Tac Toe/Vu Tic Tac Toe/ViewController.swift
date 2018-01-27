c2ee9d51ac6c08e3b63c674fa04dc06e81add36c
Tuyet <tuyet22.vu@gmail.com>
2018-01-27 18:09:59 -0500

    creating images and layout for a tic tac toe game

---
+++ Vu Tic Tac Toe/Vu Tic Tac Toe/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Vu Tic Tac Toe
+//
+//  Created by Tuyetanh Vu on 1/27/18.
+//  Copyright © 2018 Tuyetanh Vu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBAction func action(_ sender: Any) {
+        
+        
+    }
+    
+   
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
