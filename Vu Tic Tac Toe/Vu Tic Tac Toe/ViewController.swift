c62348c0b1b63d114ed5d7459ec2621d3ae70ed4
Tuyet <tuyet22.vu@gmail.com>
2018-01-27 21:19:38 -0500

    created loops, array, and conditionals

--- Vu Tic Tac Toe/Vu Tic Tac Toe/ViewController.swift
+++ Vu Tic Tac Toe/Vu Tic Tac Toe/ViewController.swift
@@ -13,45 +13,75 @@ class ViewController: UIViewController {
     var activePlayer = 1 //cross
     var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     let winCombinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
-    
     var gameActive = true
     
+@IBOutlet weak var wonLable: UILabel!
     
-    @IBAction func action(_ sender: Any) {
+@IBAction func action(_ sender: Any) {
         
-        if (gameState[(sender as AnyObject).tag-1] == 0 && gameActive == true){
+    if (gameState[(sender as AnyObject).tag-1] == 0 && gameActive == true){
             
-            gameState[(sender as AnyObject).tag-1] = activePlayer
+        gameState[(sender as AnyObject).tag-1] = activePlayer
             
-            if (activePlayer == 1)
-            {
-                (sender as AnyObject).setImage(UIImage(named: "x letter.png"), for: UIControlState())
-                activePlayer = 2
-            }else{
-                (sender as AnyObject).setImage(UIImage(named: "o letter.png"), for: UIControlState())
-                activePlayer = 1
-            }
-            
+        if (activePlayer == 1)
+        {
+            (sender as AnyObject).setImage(UIImage(named: "x letter.png"), for: UIControlState())
+            activePlayer = 2
+        }else{
+            (sender as AnyObject).setImage(UIImage(named: "o letter.png"), for: UIControlState())
+            activePlayer = 1
         }
+            
+    }
         
-        for combination in winCombinations{
-            if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]]{
+    for combination in winCombinations{
+        if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]]{
                 
-                gameActive = false
+            gameActive = false
                 
-                if gameState[combination[0]] == 1{
-                    //cross won
-                    print("cross")
-                }else{
-                    //circle won
-                    print("circle")
-                }
+            if gameState[combination[0]] == 1{
+                //cross won
+                wonLable.text = "Cross has won!"
+            }else{
+                //circle won
+                wonLable.text = "Circle has won"
+            }
+                
+            playAgainButton.isHidden = false
+            wonLable.isHidden = false
                 
             }
+        
+        }
+        gameActive = false
+        for i in gameState{
+            if i == 0 {
+                gameActive = true
+                break
+            }
+        }
+    
+    if gameActive == false{
+            wonLable.text = "IT WAS A TIE!"
+            wonLable.isHidden = false
+            playAgainButton.isHidden = false
         }
 
     }
     
+    @IBOutlet weak var playAgainButton: UIButton!
+    @IBAction func playAgain(_ sender: Any) {
+        gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        gameActive = true
+        activePlayer = 1
+        playAgainButton.isHidden = true
+        wonLable.isHidden = true
+        
+        for i in 1...9{
+            let button = view.viewWithTag(i) as! UIButton
+            button.setImage(nil, for: UIControlState())
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
