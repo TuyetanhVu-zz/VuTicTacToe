eab2cfd080790bb461b590753ec1e1a0694945c3
Tuyet <tuyet22.vu@gmail.com>
2018-01-30 23:44:54 -0500

    added constraints and a boolean to detect when game is finished

--- Vu Tic Tac Toe/Vu Tic Tac Toe/ViewController.swift
+++ Vu Tic Tac Toe/Vu Tic Tac Toe/ViewController.swift
@@ -15,10 +15,19 @@ class ViewController: UIViewController {
     let winCombinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
     var gameActive = true
     
+    var isGameFinished:Bool = false
+    
 @IBOutlet weak var wonLable: UILabel!
     
+// presses x or o button
 @IBAction func action(_ sender: Any) {
-        
+    
+    // if game is finished
+    if(isGameFinished){
+        // dont go further
+        return
+    }
+    
     if (gameState[(sender as AnyObject).tag-1] == 0 && gameActive == true){
             
         gameState[(sender as AnyObject).tag-1] = activePlayer
@@ -33,7 +42,7 @@ class ViewController: UIViewController {
         }
             
     }
-        
+    
     for combination in winCombinations{
         if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]]{
                 
@@ -42,9 +51,11 @@ class ViewController: UIViewController {
             if gameState[combination[0]] == 1{
                 //cross won
                 wonLable.text = "Cross has won!"
+                isGameFinished = true
             }else{
                 //circle won
                 wonLable.text = "Circle has won"
+                isGameFinished = true
             }
                 
             playAgainButton.isHidden = false
@@ -61,16 +72,17 @@ class ViewController: UIViewController {
             }
         }
     
-    if gameActive == false{
+        if gameActive == false{
             wonLable.text = "IT WAS A TIE!"
             wonLable.isHidden = false
             playAgainButton.isHidden = false
+            isGameFinished = true
         }
 
     }
     
-    @IBOutlet weak var playAgainButton: UIButton!
-    @IBAction func playAgain(_ sender: Any) {
+    func resetGame(){
+        isGameFinished = false
         gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
         gameActive = true
         activePlayer = 1
@@ -81,6 +93,12 @@ class ViewController: UIViewController {
             let button = view.viewWithTag(i) as! UIButton
             button.setImage(nil, for: UIControlState())
         }
+    }
+    
+    @IBOutlet weak var playAgainButton: UIButton!
+    
+    @IBAction func playAgain(_ sender: Any) {
+        resetGame()
     }
     
     override func viewDidLoad() {
